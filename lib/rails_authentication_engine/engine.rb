e503a704fc01c405c9663366714980f53ab095a3
Michael Pope <mpope.cr@gmail.com>
2015-12-21 17:39:07 -0500

    wip

--- lib/rails_authentication_engine/engine.rb
+++ lib/rails_authentication_engine/engine.rb
@@ -1,5 +1,9 @@
 module RailsAuthenticationEngine
   class Engine < ::Rails::Engine
     isolate_namespace RailsAuthenticationEngine
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+    end
   end
 end
