8739f40e90d3bd375014cb1b08c86c7f17b65860
Michael Pope <mpope.cr@gmail.com>
2015-12-21 06:33:21 -0500

    wip

--- app/controllers/rails_authentication_engine/sign_up_passwords_controller.rb
+++ app/controllers/rails_authentication_engine/sign_up_passwords_controller.rb
@@ -1,21 +1,22 @@
 module RailsAuthenticationEngine
   class SignUpPasswordsController < ApplicationController
     def create
-      @user = User.find_by(reset_password_token: session[:reset_password_token])
+      reset = PasswordResetUser.find_by(token: session[:password_reset_token])
 
-      if @user && @user.password_reset_token == params[:token]
+      if DateTime.now.utc.to_f - reset.created_at.to_f > 86400
+
       end
     end
 
     def new
       if EmailConfirmation.exists?(token: params[:token])
         confirmation = EmailConfirmation.find_by(token: params[:token])
-        if DateTime.now.utc.to_f - confirmation.created_at.to_f > 86400
+        if (DateTime.now.utc.to_f - confirmation.created_at.to_f) > 86400
           flash[:error] = "Expired email confirmation link.  Please enter your email to receive another one."
           redirect_to new_sign_up_email_path
         else
           reset = PasswordReset.create(token: SecureRandom.urlsafe_base64(24))
-          session[:password_reset_token] = PasswordReset.create(token: reset.token)
+          session[:password_reset_token] = reset.token
           render :new
         end
       else
