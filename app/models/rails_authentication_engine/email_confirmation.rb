3acbaabb1c4a9d5edb543c4b30a13bfae7b1bc2d
Michael Pope <mpope.cr@gmail.com>
2015-12-22 09:22:57 -0500

    lol

--- app/models/rails_authentication_engine/email_confirmation.rb
+++ app/models/rails_authentication_engine/email_confirmation.rb
@@ -1,4 +1,15 @@
 module RailsAuthenticationEngine
   class EmailConfirmation < ActiveRecord::Base
+    after_initialize :initialize_token
+
+    has_many :password_resets
+
+    validates :email, presence: true
+
+    private
+
+    def initialize_token
+      self.token ||= SecureRandom.urlsafe_base64(24)
+    end
   end
 end
--- app/models/rails_authentication_engine/password_reset.rb
+++ app/models/rails_authentication_engine/password_reset.rb
@@ -1,4 +1,13 @@
 module RailsAuthenticationEngine
   class PasswordReset < ActiveRecord::Base
+    after_initialize :initialize_token
+
+    belongs_to :email_confirmation
+
+    private
+
+    def initialize_token
+      self.token ||= SecureRandom.urlsafe_base64(24)
+    end
   end
 end
