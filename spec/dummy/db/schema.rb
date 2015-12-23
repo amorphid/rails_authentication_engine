ec75d8f4dc9ede6ac0cded2b41834058cf00bbc7
Michael Pope <mpope.cr@gmail.com>
2015-12-23 08:32:18 -0500

    lol

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222142029) do
+ActiveRecord::Schema.define(version: 20151223113247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20151222142029) do
     t.uuid     "email_confirmation_id"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+  end
+
+  create_table "users", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
