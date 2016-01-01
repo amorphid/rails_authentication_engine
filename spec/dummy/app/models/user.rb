5297130aba32d387f884cd8b326bdb3524a9cc46
Michael Pope <mpope.cr@gmail.com>
2016-01-01 12:43:50 -0500

    wip

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,11 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8 }, if: :validate_password?
-
-  private
-
-  def validate_password?
-    password.present?
-  end
+  validates :password, length: { minimum: 8 }
 end
