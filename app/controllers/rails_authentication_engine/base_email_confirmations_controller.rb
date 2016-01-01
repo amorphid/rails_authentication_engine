3476951d79829f557781423724c0de9659d187d3
Michael Pope <mpope.cr@gmail.com>
2016-01-01 12:41:08 -0500

    wip

--- app/controllers/rails_authentication_engine/base_email_confirmations_controller.rb
+++ app/controllers/rails_authentication_engine/base_email_confirmations_controller.rb
@@ -2,7 +2,7 @@ module RailsAuthenticationEngine
   class BaseEmailConfirmationsController < RailsAuthenticationEngine::ApplicationController
     prepend_before_action :authenticate_guest!
 
-    before_action :set_email_confirmation, only: :create
+    before_action :vet_and_set_email_confirmation, only: :create
 
     def create
       if @email_confirmation.update(email_confirmation_params)
@@ -20,8 +20,16 @@ module RailsAuthenticationEngine
 
     private
 
-    def set_email_confirmation
-      @email_confirmation ||= EmailConfirmation.find_or_initialize_by(email: params[:email])
+    def vet_and_set_email_confirmation
+      email_confirmation            = EmailConfirmation.find_or_initialize_by(email: params[:email])
+      email_confirmation_is_expired = (DateTime.now.utc.to_f - email_confirmation.created_at.to_f) >= 86400
+
+      @email_confirmation ||= if email_confirmation_is_expired
+        email_confirmation.destroy
+        EmailConfirmation.new(email: params[:email])
+      else
+        email_confirmation
+      end
     end
 
     def email_confirmation_params
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,5 +2,11 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }, if: :validate_password?
+
+  private
+
+  def validate_password?
+    password.present?
+  end
 end
