0bb043098bb37df9f89c2ce683d09ce8475d7920
Michael Pope <mpope.cr@gmail.com>
2016-01-03 11:08:29 -0500

    wip

---
+++ spec/lib/rails_authentication_engine/sign_in/manager_spec.rb
@@ -0,0 +1,11 @@
+# require 'rails_helper'
+
+# module RailsAuthenticationEngine
+#   module SignIn
+#     describe Manager do
+#       subject { Object.new.extend() }
+
+#       context
+#     end
+#   end
+# end
