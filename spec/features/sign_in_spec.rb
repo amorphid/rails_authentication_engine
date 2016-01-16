fa8b614d235c41f238bf907fdd0bbfd5cb273892
Michael Pope <mpope.cr@gmail.com>
2016-01-16 14:04:57 -0500

    wip

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,6 +6,7 @@ feature 'Sign In' do
   context 'w/ valid email and password' do
     scenario 'takse user who visited sign in path to root path' do
       visit rails_authentication_engine.new_sign_in_path
+      expect(page.body).not_to have_css('.alert-danger')
       fill_in :email, with: user.email
       fill_in :password, with: user.password
       click_button 'Submit'
@@ -23,15 +24,41 @@ feature 'Sign In' do
       click_button 'Submit'
       expect(current_path).to eq(main_app.root_path)
     end
-  end
 
-  context 'w/ valid email & password' do
-    scenario 'takes user to page' do
+    scenario 'takes user who visited pagey_mc_page_path to pagey_mc_page_path' do
       visit pagey_mc_page_path
+      within('.alert-danger') do
+        expect(page.body)
+        .to have_content(t('rails_authentication_engine.authentication.sign_in_required'))
+      end
       fill_in :email, with: user.email
       fill_in :password, with: user.password
       click_button 'Submit'
-      expect(page.body).to have_content('Pagey McPage')
+      expect(current_path).to eq(main_app.pagey_mc_page_path)
+    end
+  end
+
+  context 'w/ invalid email' do
+    scenario 'enables user to follow link to start sign up process' do
+      visit rails_authentication_engine.new_sign_in_path
+      fill_in :email, with: Faker::Internet.email
+      fill_in :password, with: user.password
+      click_button 'Submit'
+      within('.alert-danger') do
+        click_link('here')
+      end
+      expect(current_path).to eq(rails_authentication_engine.new_sign_up_email_path)
+    end
+
+    scenario 'enables user to follow link to start sign up process' do
+      visit rails_authentication_engine.new_sign_in_path
+      fill_in :email, with:    user.email
+      fill_in :password, with: Faker::Internet.password
+      click_button 'Submit'
+      within('.alert-danger') do
+        click_link('here')
+      end
+      expect(current_path).to eq(rails_authentication_engine.new_password_recovery_email_path)
     end
   end
 end
